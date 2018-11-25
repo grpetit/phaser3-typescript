5f0d66c50218f0691e018ab6f99871cb0d0b0e53
Gregory <grpetit@gmail.com>
2018-11-25 11:11:30 +0100

    bigbang

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 var phaser = path.join(pathToPhaser, 'dist/phaser.js');
 
 module.exports = {
-  entry: './src/boilerplate/game.ts',
+//  entry: './src/boilerplate/game.ts',
+  entry: './src/games/flappyBird/game.ts',
+
+
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
